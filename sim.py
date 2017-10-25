7f9de7751a9db80db6d854768b3ec601c11f7046
Forrest Voight <forrest@forre.st>
2017-10-25 13:25:39 -0400

    old

--- sim.py
+++ sim.py
@@ -100,9 +100,14 @@ class MotorModel(object):
     def __init__(self):
         self.omega = 0 # electrical angular velocity
         self.theta = 0 # electrical angle
+        self.alpha = 0
         
         self.i_0 = 0 # current into port 0
         self.i_1 = 0 # current into port 1
+        self.i_2 = 0
+        
+        self.work = 0
+        self.heat = 0
     
     def step(self, dt, v0, v1, v2): # v? can be ranges to signal that they're clamped but otherwise high-Z
         assert isinstance(v0, tuple) + isinstance(v1, tuple) + isinstance(v2, tuple) <= 1
@@ -113,10 +118,12 @@ class MotorModel(object):
         tau += self.i_1 / self.K_v * math.sin(self.theta - 2*math.pi/3)
         i_2 = - self.i_0 - self.i_1 # i_0 + i_1 + i_2 = 0
         tau += i_2 / self.K_v * math.sin(self.theta - 2*math.pi*2/3)
-        #tau += -1e-6 * self.omega * abs(self.omega)
+        air_resistance = - 1e-8 * self.omega * abs(self.omega)
+        tau_total = tau + air_resistance
         
         self.theta += dt * self.omega
-        self.omega += dt * tau/self.I
+        self.omega += dt * tau_total/self.I
+        self.alpha = tau_total/self.I
         
         vb_0 = 1/self.K_v * self.omega * math.sin(self.theta)
         vb_1 = 1/self.K_v * self.omega * math.sin(self.theta - 2*math.pi/3)
@@ -124,31 +131,58 @@ class MotorModel(object):
         
         (self.i_0, self.i_1, self.i_2), vfinal = simulate(dt, v0, v1, v2, vb_0, vb_1, vb_2, self.R, self.i_0, self.i_1, i_2, self.L)
         self.voltages = vfinal
+        
+        self.work += -air_resistance * (dt * self.omega)
+        self.heat += dt * self.i_0**2 * self.R
+        self.heat += dt * self.i_1**2 * self.R
+        self.heat += dt * self.i_2**2 * self.R
 
 m = MotorModel()
 
 vmax = 3.7*4
 sign = lambda x: 1 if x >= 0 else -1
+error_integral = 0
 def controller():
+    global error_integral
     #ang = 40*t**2
+    off = 0
     ang = m.theta
-    vs = (
-        vmax*(.5+.5*math.sin(ang - 2*math.pi*0/3)),
-        vmax*(.5+.5*math.sin(ang - 2*math.pi*1/3)),
-        vmax*(.5+.5*math.sin(ang - 2*math.pi*2/3)),
+    error = 2000 - m.omega
+    error_integral += dt * error
+    power = .1*error + 1*error_integral #+ .001*-m.alpha
+    powers.append(power)
+    
+    i_d = (
+        power*math.sin(ang - 2*math.pi*0/3 + off),
+        power*math.sin(ang - 2*math.pi*1/3 + off),
+        power*math.sin(ang - 2*math.pi*2/3 + off),
     )
-    if t * 10000 % 1 < 3/4:
-        return [0 if vs[i] == min(vs) else vmax if vs[i] == max(vs) else (0, vmax) for i in xrange(3)]
+    if 1: # current feedback
+        return (
+            vmax if m.i_0 < i_d[0] else 0,
+            vmax if m.i_1 < i_d[1] else 0,
+            vmax if m.i_2 < i_d[2] else 0,
+        )
     else:
-        return [vmax if vs[i] == min(vs) else 0 if vs[i] == max(vs) else (0, vmax) for i in xrange(3)]
+        vs = (
+            power*vmax*(.5+.5*math.sin(ang - 2*math.pi*0/3 + off)),
+            power*vmax*(.5+.5*math.sin(ang - 2*math.pi*1/3 + off)),
+            power*vmax*(.5+.5*math.sin(ang - 2*math.pi*2/3 + off)),
+        )
+        return vs
+        if t * 10000 % 1 < 3/4:
+            return [0 if vs[i] == min(vs) else power*vmax if vs[i] == max(vs) else (0, power*vmax) for i in xrange(3)]
+        else:
+            return [power*vmax if vs[i] == min(vs) else 0 if vs[i] == max(vs) else (0, power*vmax) for i in xrange(3)]
 
-dt = 1e-5
-tmax = 1
+dt = 1e-6
+tmax = 0.2
 ts = []
 thetas = []
 omegas = []
 voltages = []
 currents = []
+powers = []
 for i in xrange(int(tmax//dt)):
     t = dt * i
     ts.append(t)
@@ -159,25 +193,31 @@ for i in xrange(int(tmax//dt)):
     voltages.append(m.voltages)
     currents.append((m.i_0, m.i_1, m.i_2))
 
+print m.work, m.heat, m.work/(m.work + m.heat)
+
 
 from matplotlib import pyplot
 
-ax1 = pyplot.subplot(4, 1, 1)
+ax1 = pyplot.subplot(5, 1, 1)
 ax1.plot(ts, omegas)
 ax1.set_ylabel('Omega')
 
-ax1 = pyplot.subplot(4, 1, 2, sharex=ax1)
+ax1 = pyplot.subplot(5, 1, 2, sharex=ax1)
 ax1.plot(ts, thetas)
 ax1.set_ylabel('Theta')
 
-ax2 = pyplot.subplot(4, 1, 3, sharex=ax1)
+ax2 = pyplot.subplot(5, 1, 3, sharex=ax1)
 for v in zip(*voltages):
     ax2.plot(ts, v)
 ax2.set_ylabel('Voltages')
 
-ax2 = pyplot.subplot(4, 1, 4, sharex=ax1)
+ax2 = pyplot.subplot(5, 1, 4, sharex=ax1)
 for i in zip(*currents):
     ax2.plot(ts, i)
 ax2.set_ylabel('Currents')
+
+ax2 = pyplot.subplot(5, 1, 5, sharex=ax1)
+ax2.plot(ts, powers)
+ax2.set_ylabel('Power')
 
 pyplot.show()
