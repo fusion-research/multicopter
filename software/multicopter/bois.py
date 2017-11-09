7d96f0a3c8672044cbdc916d2b6790cba7deff87
jnez71 <nez.jason7@gmail.com>
2017-11-08 21:42:02 -0500

    sim: allow gamepad user input

--- software/multicopter/bois.py
+++ software/multicopter/bois.py
@@ -3,6 +3,7 @@ A quadcopter model and a hexcopter model are configured here for easy access and
 
 """
 from __future__ import division
+from collections import OrderedDict
 import numpy as np; npl = np.linalg
 
 from model import Model
@@ -28,19 +29,19 @@ effort_from_thrust = lambda thrust: np.sign(thrust) * np.sqrt(np.abs(thrust) / e
 reaction_coeff = 0.25 * prop_dia  # (N*m)/N
 
 # Quadcopter thrusters (sign of reaction_coeff determines handedness)
-quad_thrusters = {"front": Thruster(( r,  0, 0.02), thrust_from_effort, effort_from_thrust, reaction_coeff, max_rpm),
-                  "back":  Thruster((-r,  0, 0.02), thrust_from_effort, effort_from_thrust, reaction_coeff, max_rpm),
-                  "left":  Thruster(( 0,  r, 0.02), thrust_from_effort, effort_from_thrust, -reaction_coeff, max_rpm),
-                  "right": Thruster(( 0, -r, 0.02), thrust_from_effort, effort_from_thrust, -reaction_coeff, max_rpm)}
+quad_thrusters = OrderedDict([("front", Thruster(( r,  0, 0.02), thrust_from_effort, effort_from_thrust, reaction_coeff, max_rpm)),
+                              ("back",  Thruster((-r,  0, 0.02), thrust_from_effort, effort_from_thrust, reaction_coeff, max_rpm)),
+                              ("left",  Thruster(( 0,  r, 0.02), thrust_from_effort, effort_from_thrust, -reaction_coeff, max_rpm)),
+                              ("right", Thruster(( 0, -r, 0.02), thrust_from_effort, effort_from_thrust, -reaction_coeff, max_rpm))])
 
 # Multicopter thrusters (sign of reaction_coeff determines handedness)
 R = np.array([[np.cos(2*np.pi/12), -np.sin(2*np.pi/12), 0],[np.sin(2*np.pi/12), np.cos(2*np.pi/12), 0], [0, 0, 1]])
-hex_thrusters = {"fl": Thruster(npl.matrix_power(R, 1).dot([r, 0, 0.02]), thrust_from_effort, effort_from_thrust, reaction_coeff, max_rpm),
-                 "ml": Thruster(npl.matrix_power(R, 3).dot([r, 0, 0.02]), thrust_from_effort, effort_from_thrust, -reaction_coeff, max_rpm),
-                 "bl": Thruster(npl.matrix_power(R, 5).dot([r, 0, 0.02]), thrust_from_effort, effort_from_thrust, reaction_coeff, max_rpm),
-                 "br": Thruster(npl.matrix_power(R, 7).dot([r, 0, 0.02]), thrust_from_effort, effort_from_thrust, -reaction_coeff, max_rpm),
-                 "mr": Thruster(npl.matrix_power(R, 9).dot([r, 0, 0.02]), thrust_from_effort, effort_from_thrust, reaction_coeff, max_rpm),
-                 "fr": Thruster(npl.matrix_power(R, 11).dot([r, 0, 0.02]), thrust_from_effort, effort_from_thrust, -reaction_coeff, max_rpm)}
+hex_thrusters = OrderedDict([("fl", Thruster(npl.matrix_power(R, 1).dot([r, 0, 0.02]), thrust_from_effort, effort_from_thrust, reaction_coeff, max_rpm)),
+                             ("ml", Thruster(npl.matrix_power(R, 3).dot([r, 0, 0.02]), thrust_from_effort, effort_from_thrust, -reaction_coeff, max_rpm)),
+                             ("bl", Thruster(npl.matrix_power(R, 5).dot([r, 0, 0.02]), thrust_from_effort, effort_from_thrust, reaction_coeff, max_rpm)),
+                             ("br", Thruster(npl.matrix_power(R, 7).dot([r, 0, 0.02]), thrust_from_effort, effort_from_thrust, -reaction_coeff, max_rpm)),
+                             ("mr", Thruster(npl.matrix_power(R, 9).dot([r, 0, 0.02]), thrust_from_effort, effort_from_thrust, reaction_coeff, max_rpm)),
+                             ("fr", Thruster(npl.matrix_power(R, 11).dot([r, 0, 0.02]), thrust_from_effort, effort_from_thrust, -reaction_coeff, max_rpm))])
 
 # Model objects
 quadboi = Model(quad_thrusters, mass, inertia, drag_lin, drag_ang)
